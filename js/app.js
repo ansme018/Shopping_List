05e539315bc36d35d6d6fb19e05555b67ee6ed94
melanselmo@gmail.com <melissaanselmo@melissas-air.bidmc.harvard.edu>
2015-11-16 17:58:30 -0500

    First commit

--- js/app.js
+++ js/app.js
@@ -1,28 +1,37 @@
 
-//1. Add Item to Shopping List 
 
-	//1.1 To enable user to press "enter" to add a new item to the list. 
-	//input id#new-item
-	//keydown()
 $(document).ready(function () {
-	function NewItem() {
-		$('#new-item').keydown(function(enter){
-			if(enter.keyCode ==13) {
-				ListItem();
-		}
-	});
-}
+	//1. Add Item to Shopping List 
 
-	//1.2  To append a new item to the list
-	//input id#list-items
-	//prepend() - Insert content into another element on page
-function ListItem(){
-	var item =$('#new-item').val();
-	var OnList=$('<li>' + item + '</li>');
-	$('#new-item').prepend($('#list-items'));
-}
+	//1.1 User types in a new item.  
+
+
+	//1.2 User presses the button submit. 
+
+	$('.grocerylist form .submit').on("click", function(event){
+		event.preventDefault();
+		var newItem =$('#new-item').val();
+		console.log(newItem); 
+		addItem(newItem);
+	});
+
+
+//1.2  To append a new item to the list
+
+	function addItem(newItem) {
+		var listItem = '<li> <input id="list-items" type = "text" />' + newItem + '<button class ="gotIt"> "Got It!" </span></li>' + '<button class=\'nevermind\'>Nevermind </button>';
+		$('ul').append(listItem);
+		$('ul').bind('click', function() {
+			$(this).parent().remove();
+		})
+	}
 
 //2.  When user locates item on shopping trip or "gets it"
+
+$(document).on('.gotIt', 'button[type=checkbox]', function() {
+	$(this).parent().toggleClass('gotIt');
+});
+
 	//2.1 The text is crossed off. 
 	//2.2. The text changes color (and background of item)
 	//2.3 The text box moves to the bottom of list
@@ -31,5 +40,10 @@ function ListItem(){
 	//3.1 (Above rules are eliminated/reversed)
 
 
-//4. User realizes he/she already has the item.  Wants it removed from list. 
-	//4.1 The text is cleared from list. 
+//4. User realizes he/she already has the item.  Needs to delete an item
+
+ $('span.delete').on('click', function() {
+ 	$(this).parent().remove();
+ })
+
+});
