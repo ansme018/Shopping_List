9377478f59b46cd0c6e044877440429730c4fa82
melanselmo@gmail.com <melissaanselmo@melissas-air.bidmc.harvard.edu>
2015-11-17 17:56:03 -0500

    Shopping List JS Edits

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 
+var newItem=$('#new-item').val();
+var listItem= '<li> <input id="list-items" type = "text" />' + newItem + 
+'<button class ="gotIt"> "Yum, I got it!" </span></li>' + '<button class=\'nevermind\'>
+Nevermind </button>';
 
 $(document).ready(function () {
 	//1. Add Item to Shopping List 
-
 	//1.1 User types in a new item.  
-
-
 	//1.2 User presses the button submit. 
-
 	$('.grocerylist form .submit').on("click", function(event){
 		event.preventDefault();
 		var newItem =$('#new-item').val();
@@ -15,11 +15,11 @@ $(document).ready(function () {
 		addItem(newItem);
 	});
 
-
 //1.2  To append a new item to the list
-
 	function addItem(newItem) {
-		var listItem = '<li> <input id="list-items" type = "text" />' + newItem + '<button class ="gotIt"> "Got It!" </span></li>' + '<button class=\'nevermind\'>Nevermind </button>';
+		var listItem = '<li> <input id="list-items" type = "text" />' + newItem + 
+		'<button class ="gotIt"> "Yum, I got it!" </span></li>' + '<button class=\'nevermind\'>
+		Nevermind </button>';
 		$('ul').append(listItem);
 		$('ul').bind('click', function() {
 			$(this).parent().remove();
@@ -27,23 +27,28 @@ $(document).ready(function () {
 	}
 
 //2.  When user locates item on shopping trip or "gets it"
-
-$(document).on('.gotIt', 'button[type=checkbox]', function() {
-	$(this).parent().toggleClass('gotIt');
-});
-
-	//2.1 The text is crossed off. 
-	//2.2. The text changes color (and background of item)
-	//2.3 The text box moves to the bottom of list
+	//Text is crossed off and changes color
+	$('.gotIt button .submit').on("change", "input:checkbox:", function(event) {
+		event.preventDefault();
+		var item=$(this).closest("li").find("span");
+			if($(this).is(":checked")) {
+				item.addClass("gotIt");
+				$(this).css({
+					'color': 'blue',
+					'text-decoration': 'line-through',
+				})
+			}
+			else
+				item.removeClass("gotIt");
+		});
 
 //3.   When user unchecks "Got It!" (user made a mistake in checking it)
 	//3.1 (Above rules are eliminated/reversed)
 
 
 //4. User realizes he/she already has the item.  Needs to delete an item
-
- $('span.delete').on('click', function() {
- 	$(this).parent().remove();
+ 	$('.nevermind button .submit').on('click', function(event) {
+ 		$(this).parent().remove(); 
  })
 
 });
